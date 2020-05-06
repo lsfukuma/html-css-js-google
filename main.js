a27b842cb1d5e2cdaa3d83a97000520ffbec91d1
Leticia <lsf@protonmail.ch>
2020-05-06 12:22:02 +0200

    menu si apre e si chiude

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 //fare aprire il sottomenu
 //quando l'utente clicca sull icona
-$('.fa-th').click(function(){
-    $('.menu-nascosto').fadeToggle();
+// $('.fa-th').click(function(){
+//     $('.menu-nascosto').fadeToggle();
+// });
+
+
+//piu generico
+
+$('.header-right > ul > li > a').click(function(){
+    $(this).next('.menu-nascosto').toggle()
 });
